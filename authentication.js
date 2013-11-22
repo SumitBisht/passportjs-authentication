036d33994aac047e2eb918822b40da35c23ed511
Sumit Bisht <sumitbisht1987@gmail.com>
2013-11-22 16:59:02 +0530

    refactoring: Moved authentication code into separate file

---
+++ authentication.js
@@ -0,0 +1,130 @@
+var passport = require('passport')
+, LocalStrategy = require('passport-local').Strategy
+, TwitterStrategy = require('passport-twitter').Strategy
+, FacebookStrategy = require('passport-facebook').Strategy
+, GithubStrategy = require('passport-github').Strategy
+, UserModel = require('./models/user.js')
+, config =  require('./private-settings.js')
+, mongoose = require('mongoose')
+, User = mongoose.model('User');
+
+module.exports=
+passport.use(new LocalStrategy(
+    function(username, password, done){
+        User.findOne({username: username}, function(error, user){
+        	if(error) {
+        		console.log('Error occured:'+error);
+        		return done(error);
+        	}
+        	if(!user){
+        		console.log('User not found');
+        		return done(null, false, {message:'User not found'});
+        	}
+        	if(!user.validPassword(password)){
+        		console.log('Password not correct');
+        		return done(null, false, {message:'Password does not match'});
+        	}
+        	return done(null, user);
+        });
+}
+));
+
+passport.use(new TwitterStrategy({
+    consumerKey: config.twitter.consumerKey,
+    consumerSecret: config.twitter.consumerSecret,
+    callbackURL: config.twitter.callbackURL
+  },
+  function(token, tokenSecret, profile, done) {
+    User.findOne({id:profile.id}, function(err, user) {
+      if (!err && user != null) {
+        var objectId = mongoose.Types.ObjectId
+        User.update({"_id":user["_id"]}, {$set: {modified: new Date()}}).exec();
+         }else{
+         	var user_data = new User({
+         		id: profile.id,
+         		provider: profile.provider,
+         		displayName: profile.displayName,
+         		name: profile.username,
+         		emails:{value:profile.email},
+         		created: Date.now(),
+         		modified: Date.now(),
+         		oauthtoken: token
+         	});
+         	user_data.save(function(error){
+         		if(error)
+         			console.log('Error while saving user: '+error);
+         		else
+         			console.log('User Saved successfully:'+profile.id);
+         	});
+         }
+    	return done(null, user);
+    });
+  }
+));
+
+passport.use(new FacebookStrategy({
+    clientID:config.facebook.clientID,
+    clientSecret:config.facebook.clientSecret,
+    callbackURL:config.facebook.callbackURL,
+  },
+  function(token, tokenSecret, profile, done) {
+    User.findOne({id:profile.id}, function(err, user) {
+      if (!err && user != null) {
+        var objectId = mongoose.Types.ObjectId
+        User.update({"_id":user["_id"]}, {$set: {modified: new Date()}}).exec();
+         }else{
+            var user_data = new User({
+                id: profile.id,
+                provider: profile.provider,
+                displayName: profile.first_name+' '+profile.last_name,
+                name: profile.username,
+                emails:{value:profile.email},
+                created: Date.now(),
+                modified: Date.now(),
+                oauthtoken: token
+            });
+            user_data.save(function(error){
+                if(error)
+                    console.log('Error while saving user: '+error);
+                else
+                    console.log('User Saved successfully:'+profile.id);
+            });
+         }
+        return done(null, user);
+    });
+  }
+));
+
+passport.use(new GithubStrategy({
+    clientID: config.github.clientID,
+    clientSecret: config.github.clientSecret,
+    callbackURL: config.github.callbackURL,
+    userAgent: config.github.userAgent
+  },
+  function(token, tokenSecret, profile, done) {
+    User.findOne({id:profile.id}, function(err, user) {
+      if (!err && user != null) {
+        var objectId = mongoose.Types.ObjectId
+        User.update({"_id":user["_id"]}, {$set: {modified: new Date()}}).exec();
+         }else{
+         	var user_data = new User({
+         		id: profile.id,
+         		provider: profile.provider,
+         		displayName: profile.name,
+         		name: profile.login,
+         		emails:{type:"email", value:profile.email},
+         		created: Date.now(),
+         		modified: Date.now(),
+         		oauthtoken: token
+         	});
+         	user_data.save(function(error){
+         		if(error)
+         			console.log('Error while saving user: '+error);
+         		else
+         			console.log('User Saved successfully:'+profile.id);
+         	});
+         }
+    	return done(null, user);
+    });
+  }
+));
--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 var express = require('express')
 , util = require('util')
 , passport = require('passport')
-, LocalStrategy = require('passport-local').Strategy
-, TwitterStrategy = require('passport-twitter').Strategy
-, FacebookStrategy = require('passport-facebook').Strategy
-, GithubStrategy = require('passport-github').Strategy
-, config = require('./private-settings.js')
+, auth = require('./authentication.js')
 , mongoose = require('mongoose')
 , db = mongoose.connect('mongodb://localhost:27017/auth')
 , UserModel = require('./models/user.js')
@@ -26,125 +22,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 });
 
-passport.use(new LocalStrategy(
-    function(username, password, done){
-        User.findOne({username: username}, function(error, user){
-        	if(error) {
-        		console.log('Error occured:'+error);
-        		return done(error);
-        	}
-        	if(!user){
-        		console.log('User not found');
-        		return done(null, false, {message:'User not found'});
-        	}
-        	if(!user.validPassword(password)){
-        		console.log('Password not correct');
-        		return done(null, false, {message:'Password does not match'});
-        	}
-        	return done(null, user);
-        });
-}
-));
-
-passport.use(new TwitterStrategy({
-    consumerKey: config.twitter.consumerKey,
-    consumerSecret: config.twitter.consumerSecret,
-    callbackURL: config.twitter.callbackURL
-  },
-  function(token, tokenSecret, profile, done) {
-    User.findOne({id:profile.id}, function(err, user) {
-      if (!err && user != null) {
-        var objectId = mongoose.Types.ObjectId
-        User.update({"_id":user["_id"]}, {$set: {modified: new Date()}}).exec();
-         }else{
-         	var user_data = new User({
-         		id: profile.id,
-         		provider: profile.provider,
-         		displayName: profile.displayName,
-         		name: profile.username,
-         		emails:{value:profile.email},
-         		created: Date.now(),
-         		modified: Date.now(),
-         		oauthtoken: token
-         	});
-         	user_data.save(function(error){
-         		if(error)
-         			console.log('Error while saving user: '+error);
-         		else
-         			console.log('User Saved successfully:'+profile.id);
-         	});
-         }
-    	return done(null, user);
-    });
-  }
-));
-
-passport.use(new FacebookStrategy({
-    clientID:config.facebook.clientID,
-    clientSecret:config.facebook.clientSecret,
-    callbackURL:config.facebook.callbackURL,
-  },
-  function(token, tokenSecret, profile, done) {
-    User.findOne({id:profile.id}, function(err, user) {
-      if (!err && user != null) {
-        var objectId = mongoose.Types.ObjectId
-        User.update({"_id":user["_id"]}, {$set: {modified: new Date()}}).exec();
-         }else{
-            var user_data = new User({
-                id: profile.id,
-                provider: profile.provider,
-                displayName: profile.first_name+' '+profile.last_name,
-                name: profile.username,
-                emails:{value:profile.email},
-                created: Date.now(),
-                modified: Date.now(),
-                oauthtoken: token
-            });
-            user_data.save(function(error){
-                if(error)
-                    console.log('Error while saving user: '+error);
-                else
-                    console.log('User Saved successfully:'+profile.id);
-            });
-         }
-        return done(null, user);
-    });
-  }
-));
-
-passport.use(new GithubStrategy({
-    clientID: config.github.clientID,
-    clientSecret: config.github.clientSecret,
-    callbackURL: config.github.callbackURL,
-    userAgent: config.github.userAgent
-  },
-  function(token, tokenSecret, profile, done) {
-    User.findOne({id:profile.id}, function(err, user) {
-      if (!err && user != null) {
-        var objectId = mongoose.Types.ObjectId
-        User.update({"_id":user["_id"]}, {$set: {modified: new Date()}}).exec();
-         }else{
-         	var user_data = new User({
-         		id: profile.id,
-         		provider: profile.provider,
-         		displayName: profile.name,
-         		name: profile.login,
-         		emails:{type:"email", value:profile.email},
-         		created: Date.now(),
-         		modified: Date.now(),
-         		oauthtoken: token
-         	});
-         	user_data.save(function(error){
-         		if(error)
-         			console.log('Error while saving user: '+error);
-         		else
-         			console.log('User Saved successfully:'+profile.id);
-         	});
-         }
-    	return done(null, user);
-    });
-  }
-));
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
@@ -185,8 +62,7 @@ app.get('/success', function(req, res){
 	res.send({result:'success', details:req.user});
 });
 
-app.get('/api/me', 
-  // passport.authenticate('twitter', { session: false }),
+app.get('/api/me',
   function(req, res) {
   	if(req.user!=null){
     	res.json(req.user);
